f339cd266459391e40d9368a9797b8f4b89ef61b
huxianc <928965406@qq.com>
2019-02-27 22:37:00 +0800

    很久之前的一个响应式布局页面

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+});
